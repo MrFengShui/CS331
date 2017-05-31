7c9843b3ff0d2dc60eada1c875bd34a382964eba
MrFengShui <luansongjian@outlook.com>
2017-05-30 20:06:01 -0700

    refactor code and remove othello

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -1,47 +1,72 @@
 import string
 
-class Sentence:
+class DataRow:
 
-    def __init__(self, sentence, classify):
-        self.sentence = sentence
-        self.classify = classify
+	def __init__(self, row):
+		self.row = row
 
-    def split_to_words(self):
-        self.sentence = self.sentence.translate(None, string.punctuation)
-        return self.sentence.split()
+	def word_list(self):
+		words = self.row[0].translate(None, string.punctuation)
+		return words.split()
 
-    def format_line(self):
-        return self.split_to_words(), self.classify
+	def row_feature(self):
+		return int(self.row[1][:-1])
+		
+	def build_common_words(self, dataset):
+		count, common = func_count_word(dataset), []
+		for key, value in count.items():
+			if value >= 5: common.append(key)
+		return sorted(common)
 
+class BayesData:
+
+	def __init__(self, word, idx, features):
+		self.word = word
+		self.idx = idx
+		self.features = features
+		
+class Feature:
+
+	def __init__(self, feature, label):
+		self.feature = feature
+		self.label = label
+		
 def func_load_file(name):
-    dataset = []
-    with open(name, 'r') as file:
-        for item in file:
-            line = item.split('\t')
-            dataset.append(Sentence(line[0], int(line[1][:-1])))
-    return dataset
+	dataset = []
+	with open(name, 'r') as file:
+		for item in file:
+			line = item.split('\t')
+			dataset.append(DataRow(line))
+		file.close()
+	return dataset
 
-def func_cnt_word(dataset, cnt_set = {}):
-    for data in dataset:
-        for word in data.split_to_words():
-            if word in cnt_set: cnt_set[word] += 1
-            else: cnt_set[word] = 1
-    return cnt_set
+def func_store_file(name, words, features):
+	with open(name, 'w') as file:
+		for feature in features:
+			file.write(feature)
+		file.close()
+	
+def func_count_word(dataset):
+	count = {}
+	for datarow in dataset:
+		for word in datarow.word_list():
+			if word in count: count[word] += 1
+			else: count[word] = 1
+	return count
 
-def func_fetch_cnt(cnt_set, word):
-    return cnt_set[word] if word in cnt_set else 0
+def func_build_feature(dataset):
+	features = []
+	for datarow in dataset:
+		tmp_list = []
+		for word in datarow.build_common_words(dataset):
+			tmp_list.append(1 if word in datarow.word_list() else 0)
+		features.append(Feature(tmp_list, datarow.row_feature()))
+	return features
 
-def func_build_feature(cnt_set, line, feature):
-    for word in cnt_set:
-        if word in line.split_to_words(): feature.append(1)
-        else: feature.append(0)
-    feature.append(int(line.classify))
-    return feature
-
-dataset = func_load_file('trainingSet.txt')
-cnt_set = func_cnt_word(dataset)
-features = [func_build_feature(cnt_set, data, []) for data in dataset]
-print features
-# for data in dataset:
-#     print '+++', data.sentence, data.classify
-#     print '---', data.split_to_words(), data.classify
+if __name__ == '__main__':
+	dataset = func_load_file('trainingSet.txt')
+	datarow = dataset[1]
+	print datarow.word_list()
+	print datarow.row_feature()
+	print datarow.build_common_words(dataset)
+	print func_build_feature(dataset)
